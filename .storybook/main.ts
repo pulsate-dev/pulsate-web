38830d0839fc36be146789ea7a80833fa11d3d36
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-07-18 15:40:01 +0900

    fix: add `app/components` directory to storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,8 @@ import type { StorybookConfig } from "@storybook/react-vite";
 const config: StorybookConfig = {
   stories: [
     "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../app/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   addons: [
     "@storybook/addon-onboarding",
