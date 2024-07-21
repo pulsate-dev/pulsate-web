1c4d676e09678235ddeb82730129e1bce5a10d2a
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-07-21 22:28:00 +0900

    fix: weird padding in storybook preview

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -15,7 +15,7 @@ const preview: Preview = {
   decorators: [
     (Story) => {
       return (
-        <Theme>
+        <Theme style={{ minHeight: "inherit" }}>
           <Story />
         </Theme>
       );
