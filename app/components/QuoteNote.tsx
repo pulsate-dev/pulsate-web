b7eff665bb369c25d870a0dd7a5d8da13d75b815
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-07-22 10:35:45 +0900

    feat: add QuoteNote component

---
+++ app/components/QuoteNote.tsx
@@ -0,0 +1,22 @@
+import { Box } from "@radix-ui/themes";
+import { ComponentProps } from "react";
+import Avatar from "./Avatar";
+import Note from "./Note";
+import NoteBody from "./NoteBody";
+
+type Props = ComponentProps<typeof Note>;
+
+export default function QuoteNote({ author, content }: Props) {
+  return (
+    <Box
+      ml="6"
+      style={{
+        borderStyle: "dashed",
+        borderWidth: "1px",
+      }}
+    >
+      <Avatar {...author} />
+      <NoteBody content={content} style={{ ml: "4" }} />
+    </Box>
+  );
+}
