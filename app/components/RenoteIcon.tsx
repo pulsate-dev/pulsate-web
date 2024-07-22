681457d693279c5241ed68f7c671e30ddf07cda4
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-07-22 10:35:08 +0900

    feat: add RenoteIcon tentative component

---
+++ app/components/RenoteIcon.tsx
@@ -0,0 +1,27 @@
+// ToDo: Replace it @radix-ui/icons
+// system-uiicons:retweet
+import type { SVGProps } from "react";
+
+export default function RenoteIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 21 21"
+      {...props}
+    >
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="m13.5 13.5l3 3l3-3"></path>
+        <path d="M9.5 4.5h3a4 4 0 0 1 4 4v8m-9-9l-3-3l-3 3"></path>
+        <path d="M11.5 16.5h-3a4 4 0 0 1-4-4v-8"></path>
+      </g>
+    </svg>
+  );
+}
