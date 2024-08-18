07f6ee911fffb452df7ffc40e720b60e20655d71
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-08-18 18:17:02 +0900

    feat: add LoginForm component

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,33 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Link,
+  Text,
+  TextField
+} from "@radix-ui/themes";
+import { useTranslation } from "react-i18next";
+
+export default function LoginForm() {
+  const { t } = useTranslation();
+
+  return (
+    <Flex direction="column" gap="2">
+      <Heading>{t("login")}</Heading>
+      <Box>
+        <Text>{t("idAndMailAddress")}</Text>
+        <TextField.Root />
+      </Box>
+      <Box>
+        <Text>{t("password")}</Text>
+        <TextField.Root type="password" />
+      </Box>
+      <Flex direction="column" align="center" gap="2" mx="auto">
+        <Button>{t("login")}</Button>
+        <Link>{t("register")}</Link>
+        <Link>{t("forgotPassword")}</Link>
+      </Flex>
+    </Flex>
+  );
+}
