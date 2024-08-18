ffca58bfeccddf4e335f760d5c45952a5ade37ab
Alliana <56180684+Allianaab2m@users.noreply.github.com>
2024-08-18 18:05:40 +0900

    fix: add i18n config in storybook

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,8 +1,25 @@
 import type { Preview } from "@storybook/react";
-import React from "react";
+import React, { useEffect } from "react";
 import "@radix-ui/themes/styles.css";
 import "../app/styles/font.css";
 import { Theme } from "@radix-ui/themes";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import i18nConfig from "../i18n/config";
+
+export const globalTypes = {
+  locale: {
+    name: "Locale",
+    description: "Internationalization locale",
+    toolbar: {
+      icon: "globe",
+      items: [
+        { value: "en_US", title: "English" },
+        { value: "ja_JP", title: "日本語" }
+      ],
+      showName: true
+    }
+  }
+};
 
 const preview: Preview = {
   parameters: {
@@ -14,11 +31,20 @@ const preview: Preview = {
     }
   },
   decorators: [
-    (Story) => {
+    (Story, context) => {
+      const { locale } = context.globals;
+      const { i18n } = useTranslation();
+
+      useEffect(() => {
+        i18n.changeLanguage(locale);
+      }, [locale]);
+
       return (
-        <Theme>
-          <Story />
-        </Theme>
+        <I18nextProvider i18n={i18nConfig.default}>
+          <Theme>
+            <Story />
+          </Theme>
+        </I18nextProvider>
       );
     }
   ]
